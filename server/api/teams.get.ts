cc29c6abaf2503ff0d4ac15a5a5ab2259bb38bc2
Kaleb Ercanbrack <twitcherc@gmail.com>
2023-07-03 17:11:27 -0600

    sort teams response

--- server/api/teams.get.ts
+++ server/api/teams.get.ts
@@ -1,3 +1,5 @@
+import { asc } from "drizzle-orm";
+
 export default defineEventHandler((event) => {
-  return db.select().from(teamsTable).all();
-})
+  return db.select().from(teamsTable).orderBy(asc(teamsTable.name)).all();
+});
