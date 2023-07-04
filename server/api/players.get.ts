25b404f16dc8920d5da38b3a25525455f0422ffe
Kaleb Ercanbrack <twitcherc@gmail.com>
2023-07-04 10:33:07 -0600

    search for last name and first name pairs

--- server/api/players.get.ts
+++ server/api/players.get.ts
@@ -1,4 +1,4 @@
-import { like, and, asc, sql } from "drizzle-orm";
+import { like, and, asc, sql, or } from "drizzle-orm";
 import { validateQuery, Type } from "h3-typebox";
 export default eventHandler((event) => {
   const query = validateQuery(
@@ -7,13 +7,17 @@ export default eventHandler((event) => {
       search: Type.String({ default: "" }),
     })
   );
-  const nameClause = query.search
-    ? like(playersTable.first_name, `%${query.search}%`)
-    : undefined;
+  const firstNameSearch = like(playersTable.first_name, `%${query.search}%`);
+  const lastNameSearch = like(playersTable.last_name, `%${query.search}%`);
+  const firstLastNameSearch = sql`${playersTable.first_name} || ' ' || ${playersTable.last_name} LIKE ${query.search}`;
+  const lastFirstNameSearch = sql`${playersTable.last_name} || ' ' || ${playersTable.first_name} LIKE ${query.search}`;
+
+
+  // const nameClause = query.search ? nameSearch : undefined;
   const players = db
     .select()
     .from(playersTable)
-    .where(and(nameClause))
+    .where(or(firstNameSearch, lastNameSearch, firstLastNameSearch, lastFirstNameSearch))
     .orderBy(asc(playersTable.last_name));
   const statement = players.limit(27).all();
   const { count } = db
