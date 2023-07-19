7b2d46dffa4b1b46865d3c6e427e19ff0ceb773d
Kaleb Ercanbrack <kaleb.ercanbrack@hey.com>
2023-07-19 10:29:28 -0600

    add home link

--- pages/players/[id].vue
+++ pages/players/[id].vue
@@ -3,13 +3,21 @@ const id = useRoute().params.id;
 const playerKey = `player_${id}`;
 const { data: player } = useNuxtData(playerKey);
 !player.value &&
-await useFetch(`/api/player/${id}`, {
-  key: playerKey,
-});
+  (await useFetch(`/api/player/${id}`, {
+    key: playerKey,
+  }));
 defineOgImage({
   title: `${player?.first_name} ${player?.last_name}`,
 });
 </script>
 <template>
-  <PlayerPreview class="mx-12 mt-12" :player-id="id" />
+  <div class="m-12">
+    <NuxtLink
+      to="/"
+      class="text-primary text-lg inline-flex items-center gap-2"
+    >
+      <span class="i-tabler-arrow-left w-5 h-5"></span> Home</NuxtLink
+    >
+    <PlayerPreview :player-id="id" />
+  </div>
 </template>
